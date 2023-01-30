3e21ec36bcb82a1e71615651f7236386c31b9e77
Hieu Do <hieu.do@bellevuecollege.edu>
2023-01-29 17:58:24 -0800

    initial commit

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,22 @@
+package com.example;
+// Source: https://java-programming.mooc.fi/part-6/3-introduction-to-testing
+public class Calculator {
+
+    private int value;
+
+    public Calculator() {
+        this.value = 0;
+    }
+
+    public void add(int number) {
+        this.value = this.value + number;
+    }
+
+    public void subtract(int number) {
+        this.value = this.value + number;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
